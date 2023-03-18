9734a9682090b52620074323fe687123b879acb5
KhanhTran297 <giakhanh2972002@gmail.com>
2023-03-18 18:04:56 +0700

    update userOption component

--- myapp/src/components/Home/UserOptions.jsx
+++ myapp/src/components/Home/UserOptions.jsx
@@ -9,6 +9,7 @@ const UserOptions = (props) => {
   const { removeCookie } = useCookie();
   // console.log("check", props.checkAccount);
   let check = props.check;
+  const { profileAccount } = useAccount();
   //methods
   const handleLogout = () => {
     // console.log("hello");
@@ -18,31 +19,35 @@ const UserOptions = (props) => {
   return (
     <div className="userOptions absolute z-10 right-[24px] translate-x-0 translate-y-[10px] bg-white shadow-userOptions rounded-5">
       {props.checkAccount ? (
-        <div className=" m-0 p-userOptions w-userOptions">
+        <div className=" m-0 p-userOptions w-max">
           <Link
             to=""
-            className="yourpage flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className="yourpage p-[10px] grid grid-cols-[auto_auto] border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
           >
-            <i className="fa-solid fa-user"></i>
-            <p className=" ml-1">{props.fullname}</p>
+            <img
+              src={profileAccount?.data.avatar}
+              className=" w-[30px] h-[30px] rounded-full"
+              alt=""
+            />
+            <p className=" ml-3">{props.fullname}</p>
           </Link>
           <Link
             to="profile"
-            className="profile flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className="profile p-[10px] grid grid-cols-[20%_80%] border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer "
           >
             <i className="fa-solid fa-user-pen"></i>
             <p className=" ml-1">Profile</p>
           </Link>
           <Link
             to=""
-            className="bookmark flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className="bookmark p-[10px] grid grid-cols-[20%_80%] border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
           >
-            <i className="fa-regular fa-bookmark"></i>
+            <i className="fa-regular fa-bookmark "></i>
             <p className=" ml-1">Bookmark</p>
           </Link>
           <Link
             onClick={handleLogout}
-            className=" flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className=" grid grid-cols-[20%_80%] p-[10px] border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
           >
             <i className="fa-solid fa-right-from-bracket"></i>
             <p className=" ml-1">Logout</p>
@@ -52,13 +57,13 @@ const UserOptions = (props) => {
         <div className=" w-userOptions_authen">
           <Link
             to="signup"
-            className="signin justify-center pt-1 pb-1 flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className="signin justify-center p-[10px] flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
           >
             <p>Sign up</p>
           </Link>
           <Link
             to="login"
-            className="profile justify-center pt-1 pb-1 flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
+            className="profile justify-center p-[10px] flex border-t-[0.5px] border-solid border-gray-400 w-full items-center hover:text-userOptions cursor-pointer"
           >
             <p>Sign in</p>
           </Link>
